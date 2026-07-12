56af47b0e72ac172facb74e3edc73de15a15c2a3
hamxaaa1 <hamzaashfaq432@gmail.com>
2026-07-12 20:29:40 +0500

    v1

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://intergro.onrender.com//api", // ✅ lowercase
+  baseURL: "https://intergro.onrender.com/api", // ✅ lowercase
   withCredentials: true,
 });
